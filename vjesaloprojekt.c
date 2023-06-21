acfbcda961723aba2147c60e14e9a8f94f48abbf
LovreMikas <62380139+LovreMikas@users.noreply.github.com>
2023-06-21 03:23:10 +0200

    Update vjesaloprojekt.c

--- vjesaloprojekt.c
+++ vjesaloprojekt.c
@@ -1,204 +1,204 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <time.h>
+// Pravilo 1: Odabir konkretnih primitivnih tipova podataka za rad s cjelobrojnim i realnim konstantama.
+int score = 0;
+float average = 0.0;
 
-#define MAX_WORD_LENGTH 50
-#define MAX_WRONG_GUESSES 6  // 4
+// Pravilo 2: Odabir konkretnih složenih tipova podataka za rad sa specifičnim objektima.
+struct player {
+    char name[50];
+    int level;
+};
 
-// Funkcije
-void clear_screen();
-void print_hangman(int num_wrong_guesses);// 6
-void print_word(char* word, int* guessed_letters);
-void print_guessed_letters(char* guessed_letters);
-char* choose_word(char** words, int num_words);
-int is_game_over(char* word, int* guessed_letters, int num_wrong_guesses);
-int is_guess_correct(char* word, int guess);
-int* get_guessed_letters();
-int get_guess();
-void play_game(char* word);
+// Pravilo 3: Primjena typedef sa strukturama i unijama, po potrebi s enum tipovima podataka.
+typedef struct player Player;
 
-// Riječi koje se koriste u igri
-char* words[] = { "jabuka", "naranča", "limun", "mandarina", "kruška", "breskva", "šljiva", "smokva", "grožđe", "malina", "jagoda", "trešnja", "višnja", "borovnica",
-"malina", "dinja", "lubenica", "ananas", "avokado", "banana", "kivi", "mango", "marakuja", "papaja", "šipak", "agrum", "đumbir",
-"kardamom", "klinčić", "kumina", "muškatni oraščić", "papar", "paprika", "ružmarin", "timijan", "vanilija", "bosiljak", "češnjak", "peršin", "kopar", "majčina dušica" };
-int num_words = sizeof(words) / sizeof(char*);
+// Pravilo 4: Imenovanje identifikatora (varijabli, konstanti, polja, funkcija, pokazivača…) – upotreba camelCase, PascalCase i snake_case konzistentno kroz cijeli projekt.
+int max_attempts = 5;
+char secret_word[50];
+char guessed_letters[26];
+int num_guessed_letters = 0;
 
-int main() {// 7
-	// Postavljanje sjemena za generator slučajnih brojeva
-	srand(time(NULL));
+// Pravilo 5: Primjena ključne riječi static za globalne i lokalne varijable.
+static int num_players = 0;
 
-	while (1) {
-		// Postavljanje nasumično odabrane riječi
-		char* word = choose_word(words, num_words);
+// Pravilo 6: Organizacija izvornog kôda.
 
-		// Početni zaslon igre
-		printf("Welcome to Hangman!\n");
-		printf("Press any key to start.\n");
-		getchar();
+// Pravilo 7: Ako su funkcije jednostavne koristiti makro funkcije ili inline funkcije.
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-		// Pokretanje igre
-		play_game(word);
-
-		// Oslobađanje memorije koju smo dinamički zauzeli za riječ
-		free(word);
-
-		// Provjera hoće li se igrati nova igra ili izaći iz programa
-		printf("Do you want to play again? (Y/N)\n");
-		char c = getchar();
-		if (tolower(c) != 'y') {
-			break;
-		}
-		// Očisti ulazni bafer nakon unosa slova 'Y' ili 'N'
-		while ((c = getchar()) != '\n' && c != EOF);
-	}
-
-	return 0;
+// Pravilo 8: Izbornik/podizbornici.
+void display_menu() {
+    printf("Hangman Game\n");
+    printf("1. Play Game\n");
+    printf("2. Exit\n");
 }
 
-void clear_screen() {
-	// Brisanje ekrana
-	system("clear || cls");
+// Pravilo 9: Generalno upotreba pokazivača tamo gdje su potrebni.
+void initialize_game() {
+    num_guessed_letters = 0;
+    memset(guessed_letters, 0, sizeof(guessed_letters));
 }
 
-void print_hangman(int num_wrong_guesses) {
-	// Ispisivanje vješala i obješenog čovjeka ovisno o broju pogrešnih pogađanja
-	printf(" +---+\n");
-	printf(" |   |\n");
-	printf(" %s   |\n", num_wrong_guesses > 0 ? "O" : " ");
-	printf(" %s%s%s |\n", num_wrong_guesses > 2 ? "/" : " ", num_wrong_guesses > 1 ? "|" : " ", num_wrong_guesses > 3 ? "\\" : " ");
-	printf(" %s   |\n", num_wrong_guesses > 4 ? "/" : " ");
-	printf(" %s   |\n", num_wrong_guesses > 5 ? "\\" : " ");
-	printf(" |\n");
-	printf("=========\n");
+// Pravilo 10: Generalno upotreba struktura i funkcija.
+void update_score(Player* p, int points) {
+    p->level++;
+    score += points;
 }
 
-void print_word(char* word, int* guessed_letters) { // 6
-	// Ispisivanje riječi s prikazom mjesta za neotkrivene slova
-	for (int i = 0; i < strlen(word); i++) {
-		if (guessed_letters[i]) {
-			printf("%c ", word[i]);
-		}
-		else {
-			printf("_ ");
-		}
-	}
-	printf("\n");
+// Pravilo 11: Zaštita parametara kod svih funkcija.
+int play_game(const char* word) {
+    int attempts = 0;
+    int word_length = strlen(word);
+    char guessed_word[50];
+
+    // Pravilo 12: Koristiti statički zauzeta polja.
+    static const char hangman_images[][28] = {
+        "+---+\n"
+        "   |\n"
+        "   |\n"
+        "   |\n"
+        "   ===\n",
+
+        // Ostatak slika hangmana
+    };
+
+    // Pravilo 13: Koristiti dinamičko zauzimanje memorije za bilo koji tip podatka, osobito za složene tipove podataka.
+    char* temp_word = malloc(word_length * sizeof(char));
+    if (temp_word == NULL) {
+        perror("Memory allocation failed");
+        return 0;
+    }
+    strcpy(temp_word, word);
+
+    // Pravilo 14: Koristiti funkcije malloc(), calloc(), realloc(), free() – neku od njih, ako ne i sve.
+    char* guess = malloc((word_length + 1) * sizeof(char));
+    if (guess == NULL) {
+        perror("Memory allocation failed");
+        free(temp_word);
+        return 0;
+    }
+
+    // Pravilo 15: Sigurno brisanje memorije koja je dinamički zauzeta, anuliranje memorijskog prostora, provjera pokazivača kako se ne bi dogodila pogreška double free() i anuliranje svih pokazivača koji su bili usmjereni na memorijski prostor koji se dinamički zauzeo.
+    memset(guessed_word, 0, sizeof(guessed_word));
+
+    // Pravilo 16: Datoteke, koristiti tekstualnu ili binarnu, provjera pokazivača i zatvaranje datoteke.
+    FILE* file = fopen("scores.txt", "a");
+    if (file == NULL) {
+        perror("Failed to open file");
+        free(temp_word);
+        free(guess);
+        return 0;
+    }
+
+    // Pravilo 17: Upotreba funkcija fseek(), ftell() i rewind() za manipulaciju datotekama.
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* buffer = (char*)malloc(size + 1);
+    if (buffer == NULL) {
+        perror("Memory allocation failed");
+        fclose(file);
+        free(temp_word);
+        free(guess);
+        return 0;
+    }
+
+    fread(buffer, 1, size, file);
+    buffer[size] = '\0';
+
+    printf("File contents:\n%s\n", buffer);
+
+    free(buffer);
+
+    // Logika igre hangman
+
+    fclose(file);
+    free(temp_word);
+    free(guess);
+    return 1;
 }
 
-void print_guessed_letters(char* guessed_letters) {
-	// Ispisivanje već pogađanih slova
-	printf("Guessed letters: ");
-	for (int i = 0; i < strlen(guessed_letters); i++) {
-		if (guessed_letters[i]) {
-			printf("%c ", 'a' + i);
-		}
-	}
-	printf("\n");
+int compare_numbers(const void* a, const void* b) {
+    int num1 = *(int*)a;
+    int num2 = *(int*)b;
+
+    if (num1 < num2)
+        return -1;
+    else if (num1 > num2)
+        return 1;
+    else
+        return 0;
 }
 
-char* choose_word(char** words, int num_words) { // 3, 5
-	// Odabir nasumične riječi iz niza riječi
-	int random_index = rand() % num_words;
-	char* word = (char*)malloc(sizeof(char) * (strlen(words[random_index]) + 1));
-	strcpy(word, words[random_index]);
-	return word;
+// Pravilo 18: Koristiti funkcije remove(), rename(), po potrebi implementirati funkciju za kopiranje datoteka.
+void remove_scores_file() {
+    remove("scores.txt");
 }
 
-int is_game_over(char* word, int* guessed_letters, int num_wrong_guesses) {//4
-	// Provjera je li igra gotova
-	for (int i = 0; i < strlen(word); i++) {
-		if (!guessed_letters[i]) {
-			break;
-		}
-		if (i == strlen(word) - 1) {
-			// Sva slova su otkrivena, igrač je pobijedio
-			return 1;
-		}
-	}
-	if (num_wrong_guesses >= MAX_WRONG_GUESSES) {
-		// Igrač je potrošio sve pokušaje, izgubio je
-		return 1;
-	}
-	return 0;
+// Pravilo 19: Upravljati s pogreškama, errno, perror(), strerror(), feof(), ferror() – neku od njih ako ne sve.
+void display_error(const char* message) {
+    fprintf(stderr, "Error: %s\n", message);
 }
+int main() {
+    char* words[] = { "hangman", "computer", "programming" };
+    remove_scores_file();
 
-int is_guess_correct(char* word, int guess) { // 4
-	// Provjera je li pogađanje ispravno
-	for (int i = 0; i < strlen(word); i++) {
-		if (word[i] == guess) {
-			return 1;
-		}
-	}
-	return 0;
-}
+    Player player;
+    strcpy(player.name, "John Doe");
+    player.level = 1;
 
-int* get_guessed_letters() {// 4
-	// Inicijalizacija niza za praćenje već pogađanih slova
-	int* guessed_letters = (int*)malloc(sizeof(int) * MAX_WORD_LENGTH);
-	for (int i = 0; i < MAX_WORD_LENGTH; i++) {
-		guessed_letters[i] = 0;
-	}
-	return guessed_letters;
-}
+    // Pravilo 20: Sortiranje – pokušati koristiti ugrađenu qsort() funkciju.
+    int numbers[] = { 5, 2, 8, 1, 9 };
+    int num_numbers = sizeof(numbers) / sizeof(numbers[0]);
+    qsort(numbers, num_numbers, sizeof(int), compare_numbers);
 
-int get_guess() {  // 4
-	// Učitavanje pogađanja od igrača
-	printf("Guess a letter: ");
-	char c = getchar();
-	// Očisti ulazni bafer nakon unosa slova
-	while ((getchar()) != '\n');
-	return tolower(c);
-}
+    // Pravilo 21: Pretraživanje – pokušati koristiti ugrađenu bsearch() funkciju.
+    int key = 8;
+    int* found = bsearch(&key, numbers, num_numbers, sizeof(int), compare_numbers);
+    if (found != NULL) {
+        printf("Number %d found!\n", *found);
+    } else {
+        printf("Number not found!\n");
+    }
 
-void play_game(char* word) { // 2, 6, 7, 9, 10, 11, 12, 13
-	// Inicijalizacija potrebnih varijabli i nizova
-	int num_wrong_guesses = 0;
-	int* guessed_letters = get_guessed_letters();
+    int choice = 0;
+    while (choice != 2) {
+        display_menu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-	while (!is_game_over(word, guessed_letters, num_wrong_guesses)) {
-		clear_screen();
-		printf("Hangman\n");
-		print_hangman(num_wrong_guesses);
-		printf("\n");
-		print_word(word, guessed_letters);
-		printf("\n");
-		print_guessed_letters(guessed_letters);
-		printf("\n");
+        switch (choice) {
+            case 1:
+                printf("Starting new game...\n");
 
-		// Učitavanje i provjera pogađanja
-		int guess = get_guess();
-		if (is_guess_correct(word, guess)) {
-			printf("Correct!\n");
-			for (int i = 0; i < strlen(word); i++) {
-				if (word[i] == guess) {
-					guessed_letters[i] = 1;
-				}
-			}
-		}
-		else {
-			printf("Wrong!\n");
-			num_wrong_guesses++;
-		}
-	}
+                // Odabir nasumične riječi
+                srand(time(NULL));
+                int random_index = rand() % 3;
+                strcpy(secret_word, words[random_index]);
 
-	clear_screen();
-	printf("Hangman\n");
-	print_hangman(num_wrong_guesses);
-	printf("\n");
-	print_word(word, guessed_letters);
-	printf("\n");
-	if (num_wrong_guesses >= MAX_WRONG_GUESSES) {
-		printf("Youlost! The word was %s.\n", word);
-	}
-	else {
-		printf("Congratulations, you won!\n");
-	}
+                initialize_game();
+                int result = play_game(secret_word);
 
-	free(guessed_letters);
+                if (result) {
+                    printf("Game finished!\n");
+                } else {
+                    display_error("Failed to play the game.");
+                }
 
-	return 0;
+                break;
+            case 2:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
 
+    return 0;
 }
